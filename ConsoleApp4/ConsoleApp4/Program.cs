712897ca7a81eec52cdcb0f609a4b54a1eba4a58
BarakpaevOlzhas <40425093+BarakpaevOlzhas@users.noreply.github.com>
2018-12-13 21:11:43 +0600

    Add files via upload

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+
+namespace DataEntity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (AllLive sqlContext = new AllLive())
+            {
+                var result = sqlContext.People.ToList();
+            }
+        }
+    }
+}
---
+++ ConsoleApp4/ConsoleApp4/SportContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+
+namespace DataEntity
+{
+    public class AllLive : DbContext
+    {
+        public AllLive()
+            : base("name=SportContext")
+        {
+        }
+
+        public DbSet<Car> Cars { set; get; }
+        public DbSet<Person> People { set;get;}
+    }   
+}
